229571aa1ca83ea0007748a20553bb2037ff0f9b
petrucci4prez <natedwarshuis@gmail.com>
2018-06-01 17:00:43 -0400

    actually add luminex and read_excel_sheets

---
+++ R/luminex.r
@@ -0,0 +1,44 @@
+#' import_luminex
+#' 
+#' Imports luminex data from the multi-sheet output given
+#' from the biorad luminex machine. All sheets are combined
+#' into one output. The input is expected to be a .xlsx file
+#'
+#' @param filepath string for the file to import
+#' @return tidy tibble with columns for sample number, well, analyte, and concentration. Note that standard curves are ignored
+
+import_luminex <- function(filepath) {
+    ## only need the 96 well data for each sheet (column a just has the
+    ## the analyte which is alreadyin the sheet name)
+    ldata <- read_excel_sheets(filepath, range = "B9:J89")
+
+    ## function to remove the standard curve, the "X" in front of
+    ## the sample number, rename the column to the analyte name,
+    ## and give back only the columns we care about
+    trim_luminex <- function(x) {
+        new_ldata <- ldata[[x]] %>%
+            slice(-1:-8) %>%
+            mutate(Type = as.numeric(str_sub(Type, 2)),
+                   `Obs Conc` = as.character(`Obs Conc`)) %>%
+            select(-3:-8)
+        colnames(new_ldata) <- c("sample", "well", x)
+        new_ldata
+    }
+
+    ## join all the lists into one happy dataset
+    ## this is when r should be haskell
+    list_join <- function(x) {
+        ## ghetto pattern matching
+        if(length(x) == 2) {
+            suppressMessages(inner_join(x[[1]], x[[2]]))
+        } else {
+            suppressMessages(inner_join(x[[1]], list_join(x[-1])))
+        }
+    }
+
+    list_join(lapply(names(ldata), trim_luminex)) %>%
+        ## tidy the data
+        gather(3:32, key="cytokine", value="conc") %>%
+        ## no need for bead regions...spaces are evil
+        mutate(cytokine = str_replace(cytokine, "\\ \\(.*\\)", ""))
+}
---
+++ R/read_excel_sheets.r
@@ -0,0 +1,14 @@
+#' read_excel_sheets
+#'
+#' reads all sheets from an excel file
+#'
+#' @param filepath to the file to be imported.
+#' @param ... args to supply to read_excel
+#' @return a list of tibbles with each index as the name of the imported sheet.
+
+read_excel_sheets <- function(filepath, ...) {
+    sheetnames <- readxl::excel_sheets(filepath)
+    workbook <- lapply(sheetnames, readxl::read_excel, path=filepath, ...)
+    names(workbook) <- sheetnames
+    workbook
+}
