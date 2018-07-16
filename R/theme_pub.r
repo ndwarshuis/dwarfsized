9bc48014989169cd83bdfed9c8bcfaa92bb4307c
petrucci4prez <natedwarshuis@gmail.com>
2018-07-16 16:37:59 -0400

    added new docs and fixed theme_pub

--- R/theme_pub.r
+++ R/theme_pub.r
@@ -4,11 +4,11 @@
 #' 
 #' @export
 
-theme_pub <- theme_classic() +
-    theme(text = element_text(size = 7),
-          line = element_line(size = 0.2),
-          axis.line = element_line(size = 0.2),
-          legend.box.spacing = unit(0.2, "cm"),
-          legend.margin = margin(0.1, 0.1, 0.1, 0.1, "cm"),
-          legend.key.size = unit(0.55, "lines"),
-          legend.spacing = unit(0.15, "cm"))
+theme_pub <- ggplot2::theme_classic() +
+    ggplot2::theme(text = ggplot2::element_text(size = 7),
+          line = ggplot2::element_line(size = 0.2),
+          axis.line = ggplot2::element_line(size = 0.2),
+          legend.box.spacing = ggplot2::unit(0.2, "cm"),
+          legend.margin = ggplot2::margin(0.1, 0.1, 0.1, 0.1, "cm"),
+          legend.key.size = ggplot2::unit(0.55, "lines"),
+          legend.spacing = ggplot2::unit(0.15, "cm"))
