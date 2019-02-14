2b176b1a1b5c37ca2ec9e1930557b0afea6e871c
ndwarshuis <natedwarshuis@gmail.com>
2019-02-14 15:50:40 -0500

    added dev to ggpub arg list

--- R/ggpup.r
+++ R/ggpup.r
@@ -9,11 +9,12 @@
 #' @param plot the plot to export (last plot if NONE)
 #' @param geoms list of geoms to change in the form list(geom1 = list(prop1 = val1), etc).
 #' @param theme the theme to use
+#' @param dev the device to use (default to "pdf")
 #' @return NONE
 #' @export
 
 ggpub <- function(filename, width, height, plot = ggplot2::last_plot(),
-                 geoms = geom_pub, theme = theme_pub) {
+                 geoms = geom_pub, theme = theme_pub, dev = "pdf") {
 
     ## update the geoms to something more sane
     gnames <- names(geoms)
@@ -22,8 +23,9 @@ ggpub <- function(filename, width, height, plot = ggplot2::last_plot(),
     lapply(gnames, function(x) ggplot2::update_geom_defaults(x, geoms[[x]]))
 
     ## save the plot with the new theme
-    ggplot2::ggsave(paste(filename, ".pdf", sep = ""), plot = plot + theme,
-           units = "mm", width = width, height = height, dev="pdf")
+    ggplot2::ggsave(paste(filename, ".pdf", sep = ""), dev = dev,
+                    plot = plot + theme, units = "mm", width = width,
+                    height = height)
 
     ## reset geoms
     gnames <- names(geoms)
